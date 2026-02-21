5c5103b716f161955fc817cbd1b65555b60d7879
ondrejsilhavy1-cmd <ondrej.silhavy1@gmail.com>
2026-02-21 15:37:36 +0100

    Fix logging format string syntax

--- brief_bot.py
+++ brief_bot.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 logging.basicConfig(
 level=logging.INFO,
-format=”%(asctime)s [%(levelname)s] %(message)s”,
+format="%(asctime)s [%(levelname)s] %(message)s",
 handlers=[logging.FileHandler(“bot.log”), logging.StreamHandler()]
 )
 log = logging.getLogger(**name**)
